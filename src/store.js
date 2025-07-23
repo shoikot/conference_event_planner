0aa7450e89a32346826be0c2f59a744445679a4f
shoikot <saikotju@hotmail.com>
2025-07-23 12:42:27 +0600

    event planner frontend

--- src/store.js
+++ src/store.js
@@ -1,9 +1,18 @@
-// store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
-
-export default configureStore({
-  reducer: {
-    venue: venueReducer,
-  },
+import { createSlice } from '@reduxjs/toolkit';
+export const mealsSlice = createSlice({
+    name: 'meals',
+    initialState: [
+        { name: 'Breakfast', cost: 50, selected: false },
+        { name: 'High Tea', cost: 25, selected: false },
+        { name: 'Lunch', cost: 65, selected: false },
+        { name: 'Dinner', cost: 70, selected: false }
+    ],
+    reducers: {
+        toggleMealSelection: (state, action) => {
+            state[action.payload].selected = !state[action.payload].selected;
+        }
+    }
 });
+
+export const { toggleMealSelection } = mealsSlice.actions;
+export default mealsSlice.reducer;
